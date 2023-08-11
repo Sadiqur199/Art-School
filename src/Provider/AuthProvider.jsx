fad8abf002c101f2d8df8174368d91229e29131c
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-08-12 02:47:21 +0600

    added login system

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -9,7 +9,6 @@ const auth = getAuth(app)
 
 const googleProvider = new GoogleAuthProvider()
 
-
 const AuthProvider = ({children}) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
